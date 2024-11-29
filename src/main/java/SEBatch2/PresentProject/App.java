1f3f8a45b4aff6e322c9765c48ee98004f358431
keerthi <keerthipalimkar244@gmail.com>
2024-11-29 14:28:32 +0530

    Demo Java

---
+++ src/main/java/SEBatch2/PresentProject/App.java
@@ -0,0 +1,10 @@
+package SEBatch2.PresentProject;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello this is keerthi and my roll no. is 23BD5A6612");
+    }
+}
